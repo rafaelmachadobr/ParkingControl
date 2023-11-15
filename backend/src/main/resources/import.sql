bf24730ba7ef332f986cbdd710e394123f506600
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-11-15 16:42:29 -0300

    feat: add owners and cars to import.sql

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO owners (id, first_name, last_name, email, phone) VALUES ('822ee87e-83ec-11ee-b962-0242ac120002', 'Diogo', 'Dias', 'diogo@email.com', '(79) 97827-4385');
+INSERT INTO owners (id, first_name, last_name, email, phone) VALUES ('875e800c-83ec-11ee-b962-0242ac120002', 'Ana', 'Souza', 'ana@email.com', '(81) 84617-5428');
+INSERT INTO owners (id, first_name, last_name, email, phone) VALUES ('8a2ef032-83ec-11ee-b962-0242ac120002', 'Rafael', 'Oliveira', 'rafael@email.com', '(17) 98728-6303');
+
+INSERT INTO cars (id, plate, model, color, brand, owner_id) VALUES ('96c7a1c8-83ec-11ee-b962-0242ac120002', 'NEX-8237', 'Sedan', 'Azul', 'Toyota', '822ee87e-83ec-11ee-b962-0242ac120002');
+INSERT INTO cars (id, plate, model, color, brand, owner_id) VALUES ('a125e4a4-83ec-11ee-b962-0242ac120002', 'KNG-2830', 'SUV', 'Preto', 'Honda', '875e800c-83ec-11ee-b962-0242ac120002');
+INSERT INTO cars (id, plate, model, color, brand, owner_id) VALUES ('a4f15774-83ec-11ee-b962-0242ac120002', 'FTD-5304', 'Hatchback', 'Branco', 'Volkswagen', '8a2ef032-83ec-11ee-b962-0242ac120002');
+INSERT INTO cars (id, plate, model, color, brand, owner_id) VALUES ('b3b4c2a6-83ec-11ee-b962-0242ac120002', 'MPP-8609', 'Caminhonete', 'Vermelho', 'Chevrolet', '822ee87e-83ec-11ee-b962-0242ac120002');
+INSERT INTO cars (id, plate, model, color, brand, owner_id) VALUES ('b7c0cf6e-83ec-11ee-b962-0242ac120002', 'JGS-7007', 'Sedan', 'Prata', 'Ford', '8a2ef032-83ec-11ee-b962-0242ac120002');
