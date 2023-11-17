e975281a9f1abd700bca69245773967bd5d5a2a1
Rafael Ferreira Machado <rafaelf.machado2004@gmail.com>
2023-11-17 12:05:56 -0300

    feat: add Sidebar component and logo image, update Tailwind config

---
+++ frontend/src/components/Sidebar.tsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+import {
+  FaArrowLeft,
+  FaCar,
+  FaChartBar,
+  FaCog,
+  FaParking,
+  FaUser,
+  FaUsers,
+} from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+interface Menu {
+  title: string;
+  icon: JSX.Element;
+  route: string;
+}
+
+const Sidebar = () => {
+  const [open, setOpen] = useState(true);
+  const menus: Menu[] = [
+    { title: "Visão Geral", icon: <FaChartBar />, route: "/" },
+    { title: "Clientes", icon: <FaUsers />, route: "/clients" },
+    { title: "Estacionamento", icon: <FaCar />, route: "/parking" },
+    { title: "Vagas", icon: <FaParking />, route: "/spots" },
+    { title: "Conta", icon: <FaUser />, route: "/account" },
+    { title: "Configurações", icon: <FaCog />, route: "/settings" },
+  ];
+
+  return (
+    <div
+      className={` ${
+        open ? "w-72" : "w-20 "
+      } bg-dark-purple h-screen p-5 pt-8 relative duration-300`}
+    >
+      <FaArrowLeft
+        className={`absolute cursor-pointer -right-3 top-9 w-7 h-7 border-dark-purple flex justify-center items-center bg-light-white border-2 rounded-full
+        ${!open && "rotate-180"}`}
+        onClick={() => setOpen(!open)}
+      />
+      <Link to="/">
+        <div className="flex gap-x-4 items-center mb-8">
+          <img
+            src="./src/assets/logo.jpeg"
+            className={`cursor-pointer duration-500 rounded-full ${
+              open && "rotate-[360deg] w-20 h-20"
+            }`}
+          />
+        </div>
+      </Link>
+      <div
+        className={`p-3 bg-white bg-opacity-5 rounded-lg justify-center items-center ${
+          !open && "hidden"
+        }`}
+      >
+        <div className="w-16 self-stretch flex-col justify-center items-start">
+          <span className="text-light-white text-2xl font-bold">
+            Sistema
+            <br />
+            Estacionamento
+          </span>
+        </div>
+      </div>
+      <ul className="mt-6 flex flex-col gap-y-2">
+        {menus.map((menu) => (
+          <li key={menu.title} title={menu.title} aria-label={menu.title}>
+            <Link
+              to={menu.route}
+              className={`flex rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 duration-200`}
+            >
+              <div
+                className={`w-7 h-7 flex justify-center items-center text-xl`}
+              >
+                {menu.icon}
+              </div>
+              <span
+                className={`${
+                  !open && "hidden"
+                } origin-left duration-200 text-sm`}
+              >
+                {menu.title}
+              </span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "dark-purple": "#081A51",
+        "light-white": "#F7F7F7",
+      },
+    },
   },
   plugins: [],
 };
